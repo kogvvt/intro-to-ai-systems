14ddad5a9f9078a9155c368e6acd1cb76b803963
kogvvt <kogvvt@icloud.com>
2023-06-08 21:08:48 +0200

    Finishing the last algorithm

--- Genetic-Algorithms/main.py
+++ Genetic-Algorithms/main.py
@@ -2,43 +2,18 @@ import random
 import numpy as np
 
 def encode_binary(x, num_bits):
-    binary_string = ""
-    integer_part = int(x)
-    fract_part = x-integer_part
-    
-    int_binary = bin(integer_part)[2:]
-    fract_binary = ''
-    while fract_part > 0 and len(fract_binary) < num_bits:
-        fract_part *= 2
-        bit = '1' if fract_part >= 1 else '0'
-        fract_binary += bit
-        fract_part -= int(fract_part)
-
-    
-    binary_string = int_binary + '.' + fract_binary
-    if len(binary_string) < num_bits+1 :
-        binary_string.ljust(len(binary_string),'0')
-
+    max = num_bits*num_bits - 1
+    step_size = 10 / max
+    scaled_x = int(x / step_size)
+    binary_string = format(scaled_x, '0' + str(num_bits) + 'b')
     return binary_string
 
-
 def decode_binary(binary_string, num_bits):
-    parts = binary_string.split('.')
-    integer_binary = parts[0]
-    fractional_binary = parts[1]
-
-    
-    integer_part = int(integer_binary, 2)
-
-    
-    fractional_part = 0
-    for i, bit in enumerate(fractional_binary):
-        if bit == '1':
-            fractional_part += 2 ** -(i + 1)
-
-    decoded_number = integer_part + fractional_part
-
-    return decoded_number
+    max = num_bits*num_bits - 1
+    step_size = 10 / max
+    scaled_x = int(binary_string, 2)
+    x = round(scaled_x * step_size,5)
+    return x
 
 num_bits = 8
 x = 7.25
--- Neural-Network/ex1/main.py
+++ Neural-Network/ex1/main.py
@@ -15,23 +15,6 @@ class Neuron:
                             # then transform the value via an activation function
         return self._f(xs @ self.ws + self.b) 
     
-    def sig(x):
-        return 1/(1 + np.exp(-x))
-   
-    def train(self, training_data, learning_rate=0.1, epochs=100):
-        for _ in range(epochs):
-            for inputs, target_output in training_data:
-                # Forward propagation
-                output = self.__call__(inputs)
-
-                # Backpropagation
-                error = target_output - output
-                delta_weights = learning_rate * error * inputs
-                delta_bias = learning_rate * error
-
-                # Update weights and bias
-                self.ws += delta_weights
-                self.b += delta_bias
                 
 class NeuralNetwork:
     def __init__(self):
@@ -48,53 +31,14 @@ class NeuralNetwork:
         output = self.output_layer(hidden_layer2_outputs)
         return output
 
-    def train(self, training_data, learning_rate=0.1, epochs=100):
-        for _ in range(epochs):
-            for inputs, target_output in training_data:
-                
-                hidden_layer1_outputs = [neuron(inputs) for neuron in self.hidden_layer1]
-                hidden_layer2_outputs = [neuron(hidden_layer1_outputs) for neuron in self.hidden_layer2]
-                output = self.output_layer(hidden_layer2_outputs)
-
-                
-                error = target_output - output
-                delta_weights_output = learning_rate * error * np.array(hidden_layer2_outputs)
-                delta_bias_output = learning_rate * error
-
-                delta_weights_hidden2 = [
-                    learning_rate * delta_w * np.array(hidden_layer1_outputs)
-                    for delta_w in self.output_layer.ws
-                ]
-                delta_bias_hidden2 = [learning_rate * delta_w for delta_w in self.output_layer.ws]
-
-                delta_weights_hidden1 = [
-                    learning_rate * delta_w * np.array(inputs)
-                    for delta_w in np.concatenate([neuron.ws for neuron in self.hidden_layer2])
-                ]
-                delta_bias_hidden1 = [learning_rate * delta_w for delta_w in np.concatenate([neuron.ws for neuron in self.hidden_layer2])]
-
-                
-                self.output_layer.ws += delta_weights_output
-                self.output_layer.b += delta_bias_output
-
-                for neuron, delta_w, delta_b in zip(self.hidden_layer2, delta_weights_hidden2, delta_bias_hidden2):
-                    neuron.ws += delta_w
-                    neuron.b += delta_b
-
-                for neuron, delta_w, delta_b in zip(self.hidden_layer1, delta_weights_hidden1, delta_bias_hidden1):
-                    neuron.ws += delta_w
-                    neuron.b += delta_b
-
     def visualize(self):
         G = nx.DiGraph()
 
-        
         G.add_nodes_from(["IL {}".format(i+1) for i in range(3)])
         G.add_nodes_from(["HL 1 {}".format(i+1) for i in range(4)])
         G.add_nodes_from(["HL 2 {}".format(i+1) for i in range(4)])
         G.add_node("OL")
 
-        
         for i in range(3):
             for j in range(4):
                 G.add_edge("IL {}".format(i+1), "HL 1 {}".format(j+1))
